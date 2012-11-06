89a8078db8004b79936408901234bb584d05c22e
rmalford2007@gmail.com <rmalford2007@gmail.com@a6b3472b-85fa-d7a3-3765-2f40e871e0e6>
2012-11-06 22:23:05 +0000

    Finished Point class. Added pendingClientList and clientsList for saving IPaddress + Port and the replyPort.

--- Project Deathmatch/src/deathmatch/main/Server.java
+++ Project Deathmatch/src/deathmatch/main/Server.java
@@ -6,12 +6,19 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Server {
 
 	// Class variables
 	int maxPlayers;
-	
+	HashMap<String, Integer> pendingClientList;  //Key: IPaddress:Port   Value: ReplyPort
+													//Holds Pending Clients that are loading the map
+	HashMap<String, Integer> clientList;  //Key: IPaddress:Port   Value: ReplyPort
+											//Holds Clients who are currently playing
+	HashMap<String, Point> playerList;	  //Key: IPaddress:Port   Value: Character Point Location
+											//Holds Client character locations
 	
 	// CONSTRUCTORS
 	
@@ -28,7 +35,7 @@ public class Server {
 	 */
 	public void start() {
 		
-		
+		serverSetup();
 		// Start the receive thread
 		Thread receiveThread = new Thread(new ReceiveThread());
 		receiveThread.start();
@@ -39,7 +46,7 @@ public class Server {
 		
 		// Read commands
 		while(true) {
-			
+			//prompt server commands
 			
 		}
 		
@@ -47,12 +54,30 @@ public class Server {
 	
 	
 	// PRIVATE METHODS
-	
 
-
+	private void serverSetup(){
+		System.out.print("Choose a map:\n" +
+							"	1 - Test\n" +
+							"	2 - Default\n" +
+							"	0 - Quit\n" +
+							"> ");
+		Scanner readIn = new Scanner(System.in);
+		readIn.nextInt();
+		if(readIn.equals(1)){
+			
+		}
+		else if(readIn.equals(2)){
+			
+		}
+		else if(readIn.equals(0)){
+			System.exit(0);
+		}
+		readIn.close();
+		
+	}//serverSetup()
 	// THREAD CLASSES
 	
-	private static class ReceiveThread
+	private class ReceiveThread
 		implements Runnable {
 		
 		byte[] receiveData = new byte[1024];
@@ -72,21 +97,41 @@ public class Server {
 					String sentence = new String(receivePacket.getData());
 	                System.out.println("RECEIVED: " + sentence);
 	                
-	                if(sentence.substring(0, 2).equals("NC")) {
-	                	// New connection received
+	                
+	                if(clientList.get(receivePacket.getAddress().toString()+Integer.toString(receivePacket.getPort())) != null){
+	                	//Packet is from client currently playing
+	                	
+	                	if(sentence.equals("TC")){
+	                		// Terminate Connection from client
+	                		clientList.remove(receivePacket.getAddress().toString()+Integer.toString(receivePacket.getPort()));
+	                	}	
 	                }
+	                else if(pendingClientList.get(receivePacket.getAddress().toString()+Integer.toString(receivePacket.getPort())) != null){
+	                	//Packet is from pending client loading the game.
+	                }
+	                else{
+	                	//Packet is from unknown client
+	                	//check if packet is a port number
+                		int replyPort = Integer.parseInt(sentence);
+                		pendingClientList.put(receivePacket.getAddress().toString()+Integer.toString(receivePacket.getPort()), replyPort);
+	                }
+	              
+	                
 				}
 			} catch (SocketException e) {
 				System.out.println("Unable to open a socket connection on specified port number.");
 				System.exit(0);
 			} catch (IOException e) {
 				System.out.println("Unable to receive packet.");
-			}
+			} catch(NumberFormatException e){
+	            System.out.println("Unable to parse port from new clients packet.");
+	            //Should this do anything here?
+	        }
 			
 		}
 	}
 	
-	private static class SendThread
+	private class SendThread
 		implements Runnable {
 		
         byte[] sendData = new byte[1024];
