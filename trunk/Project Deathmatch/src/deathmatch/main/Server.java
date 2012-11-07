226364cf7a76ab4645b832d8105a00a34a8dd263
rmalford2007@gmail.com <rmalford2007@gmail.com@a6b3472b-85fa-d7a3-3765-2f40e871e0e6>
2012-11-07 02:44:48 +0000

    Added classes folder with archer.cls, warrior.cls, mage.cls, cleric.cls Has working functionality with client talking to server using pendingClientList and clientList with states.

--- trunk/Project Deathmatch/src/deathmatch/main/Server.java
+++ trunk/Project Deathmatch/src/deathmatch/main/Server.java
@@ -15,12 +15,14 @@ public class Server {
 
 	// Class variables
 	int maxPlayers; //max number of players for selected map
+	String nextMap; //next map that the server will change to / broadcast to clients
 	HashMap<String, Integer> pendingClientList;  //Key: IPaddress:Port   Value: ReplyPort
 													//Holds Pending Clients that are loading the map
 	HashMap<String, Integer> clientList;  //Key: IPaddress:Port   Value: ReplyPort
 											//Holds Clients who are currently playing
 	HashMap<String, Point> playerList;	  //Key: IPaddress:Port   Value: Character Point Location
 											//Holds Client character locations
+	
 	
 	// CONSTRUCTORS
 	
@@ -36,11 +38,24 @@ public class Server {
 	 *  Description - Called at the start of the program
 	 */
 	public void start() {
+		//Initialize HashMaps
+		clientList = new HashMap<String, Integer>();
+		pendingClientList = new HashMap<String, Integer>();
+		playerList = new HashMap<String, Point>();
+		
+		//Get the list of available maps 
 		String[] mapList = getMaps();
 		if(mapList == null){
 			System.out.println("Unable to find maps.");
 			System.exit(0);
 		}
+		//Get the list of available character classes
+		//String[] classList = getClasses();
+		//if(classList == null){
+		//	System.out.println("Unable to find character classes.");
+		//	System.exit(0);
+		//}
+		//get the next map from the server
 		serverSetup(mapList);
 		// Start the receive thread
 		Thread receiveThread = new Thread(new ReceiveThread());
@@ -60,11 +75,15 @@ public class Server {
 	
 	
 	// PRIVATE METHODS
+	/************
+	 * DESCRIPTIONS - getMaps
+	 * Returns a list of all maps in the maps folder. 
+	 */
 	private String[] getMaps(){
-		String FILE_DIR = "\\Images\\Maps";
+		String FILE_DIR = ".\\maps";
 		FilenameFilter extFilter = new FilenameFilter() {
 			public boolean accept(File directory, String fileName) {
-			    return fileName.endsWith(".png");
+			    return fileName.endsWith(".map");
 			}
 			};
 		File mapDir = new File(FILE_DIR);
@@ -78,22 +97,58 @@ public class Server {
 			System.out.println("no files end with : " + extFilter);
 			return null;
 		}
+		else {
+			for(int x = 0; x < list.length;x++){
+				list[x] = list[x].substring(0, list[x].indexOf("."));
+			}
+		}
 		return list;
 	}
-
+	
+	/************
+	 * DESCRIPTIONS - getClasses
+	 * Returns a list of all classes from the classes folder
+	 */
+	private String[] getClasses(){
+		String FILE_DIR = ".\\classes";
+		FilenameFilter extFilter = new FilenameFilter() {
+			public boolean accept(File directory, String fileName) {
+			    return fileName.endsWith(".cls");
+			}
+			};
+		File mapDir = new File(FILE_DIR);
+		if(mapDir.isDirectory()==false){
+			System.out.println("Directory does not exists : " + FILE_DIR);
+			return null;
+		}
+		String[] list = mapDir.list(extFilter);
+		
+		if (list.length == 0) {
+			System.out.println("no files end with : " + extFilter);
+			return null;
+		}
+		else {
+			for(int x = 0; x < list.length;x++){
+				list[x] = list[x].substring(0, list[x].indexOf("."));
+			}
+		}
+		return list;
+	}
+	
+	/**********************
+	 * DESCRIPTION - serverSetup
+	 * Prompts the user for the first map 
+	 * after displaying all available maps. 
+	 * @param mapListPR
+	 */
 	private void serverSetup(String[] mapListPR){
 		System.out.println("Choose a map:");
 		for(int x = 0; x < mapListPR.length; x++){
-			System.out.println(x+1 + " - " + mapListPR[x]);
+			System.out.println(mapListPR[x]);
 		}
+		System.out.print("Enter map:");
 		Scanner readIn = new Scanner(System.in);
-		int mapInt = readIn.nextInt();
-		if(mapInt==1){
-			//Test map is chosen
-		}
-		else if(mapInt == 0){
-			System.exit(0);
-		}
+		nextMap = readIn.next();
 		readIn.close();
 		
 	}//serverSetup()
@@ -116,35 +171,47 @@ public class Server {
 				while(true) {
 					
 					//Debug
-						System.out.println("Waiting to receive...");
+					System.out.println("Waiting to receive...");
 						
 					DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 					serverSocket.receive(receivePacket);
 					String sentence = new String(receivePacket.getData());
 					
 	                //Debug
-						System.out.println("RECEIVED: " + sentence);
+					System.out.println("RECEIVED: " + sentence);
 	                
-	                
+	                //Game State
+					//Moves to Stop Connection State when TC is sent from client
 	                if(clientList.get(receivePacket.getAddress().toString()+Integer.toString(receivePacket.getPort())) != null){
 	                	//Packet is from client currently playing
 	                	
+	                	//Stop Connection State
+	                	//Removes client from list
 	                	if(sentence.equals("TC")){
 	                		// Terminate Connection from client
 	                		clientList.remove(receivePacket.getAddress().toString()+Integer.toString(receivePacket.getPort()));
 	                	}	
 	                }
+	                //Init State
+	                //Moves to Game State when clients send playerName:className
 	                else if(pendingClientList.get(receivePacket.getAddress().toString()+Integer.toString(receivePacket.getPort())) != null){
 	                	//Packet is from pending client loading the game.
+	                	
+	                	String[] sa = sentence.split(":");
+	                	
+	                	if(sa.length == 2){ //TODO check for valid class names
+	                		clientList.put(receivePacket.getAddress().toString()+Integer.toString(receivePacket.getPort()), pendingClientList.get(receivePacket.getAddress().toString()+Integer.toString(receivePacket.getPort())));
+	                		pendingClientList.remove(receivePacket.getAddress().toString()+Integer.toString(receivePacket.getPort()));
+	                	}
 	                }
+	                //NC State
+	                //Moves to Init State on valid port number sent from client
 	                else{
 	                	//Packet is from unknown client
 	                	//check if packet is a port number
-                		int replyPort = Integer.parseInt(sentence);
+                		int replyPort = Integer.parseInt(sentence.trim());
                 		pendingClientList.put(receivePacket.getAddress().toString()+Integer.toString(receivePacket.getPort()), replyPort);
 	                }
-	              
-	                
 				}
 			} catch (SocketException e) {
 				System.out.println("Unable to open a socket connection on specified port number.");
