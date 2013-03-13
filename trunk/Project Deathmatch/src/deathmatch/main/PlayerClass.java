fb64eb9db91d3e33fa9eb94459428c7564f53449
schwissig <schwissig@a6b3472b-85fa-d7a3-3765-2f40e871e0e6>
2013-03-13 01:27:07 +0000

    Added content to '.cls' files and loaded the PlayerClasses into client

--- trunk/Project Deathmatch/src/deathmatch/main/PlayerClass.java
+++ trunk/Project Deathmatch/src/deathmatch/main/PlayerClass.java
@@ -1,5 +1,55 @@
 package deathmatch.main;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class PlayerClass {
 
+	private String classType;
+	private int health, energy, armor, speed;
+	private Sprite sprite;
+	
+	
+	public PlayerClass() {
+		
+		
+	}
+	
+	static PlayerClass loadPlayerClass(String filename) {
+		
+		PlayerClass playerClass = new PlayerClass();
+		
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(new File(filename)));
+			String line;
+			String[] sa;
+			
+			while((line = br.readLine()) != null) {
+				sa = line.split("=");
+				sa[0] = sa[0].trim();
+				sa[1] = sa[1].trim();
+				
+				if(sa[0].equals("type")) {
+					playerClass.classType = br.readLine().trim();
+				} else if(sa[0].equals("health")) {
+					playerClass.health = Integer.parseInt(br.readLine().split("=")[1].trim());
+				} else if(sa[0].equals("energy")) {
+					playerClass.energy = Integer.parseInt(br.readLine().split("=")[1].trim());
+				} else if(sa[0].equals("armor")) {
+					playerClass.armor = Integer.parseInt(br.readLine().split("=")[1].trim());
+				} else if(sa[0].equals("speed")) {
+					playerClass.speed = Integer.parseInt(br.readLine().split("=")[1].trim());
+				}
+			}
+			
+			return playerClass;
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			
+			return null;
+		}
+	}
 }
